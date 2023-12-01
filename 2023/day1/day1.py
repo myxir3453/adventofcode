c9009c6323198b2d444161c19364ce72cebf905f
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-01 20:28:36 +0200

    day 1, puzzle 2 complete

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -2,23 +2,26 @@ fi = open('input', 'r')
 
 total = 0
 
+number_constants = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine' , '1', '2', '3', '4', '5', '6', '7', '8', '9']
+number_constants_toint = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9 }
+
 for line in fi:
-    first_number = -1
-    second_number = -1
+    ltor_min = len(line)
+    ltor_value = ''
+    rtol_max = -1
+    rtol_value = ''
+
+    for number_constant in number_constants:
+        position_ltor = line.find(number_constant)
+        if position_ltor != -1 and position_ltor < ltor_min:
+            ltor_min = position_ltor
+            ltor_value = number_constants_toint[number_constant]
+        
+        position_rtol = line.rfind(number_constant)
+        if position_rtol != -1 and position_rtol > rtol_max:
+            rtol_max = position_rtol
+            rtol_value = number_constants_toint[number_constant]
     
-    for char in line:
-        if char.isdigit():
-            if first_number == -1:
-                first_number = int(char)
-                second_number = int(char)
-            else:
-                second_number = int(char)
-            
-    if first_number == -1 and second_number == -1:
-        first_number = 0
-        second_number = 0
-        print('no numbers found for ' + line)
-    
-    total += (first_number * 10) + second_number
+    total += (ltor_value * 10) + rtol_value
 
 print(total)
