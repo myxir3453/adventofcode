37d272d74fab48c272934d94ab8de3b6ac6262b3
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-01 20:07:38 +0200

    day 1, puzzle 1 complete

--- 2023/day1/day1.py
+++ 2023/day1/day1.py
@@ -1,1 +1,24 @@
+fi = open('input', 'r')
 
+total = 0
+
+for line in fi:
+    first_number = -1
+    second_number = -1
+    
+    for char in line:
+        if char.isdigit():
+            if first_number == -1:
+                first_number = int(char)
+                second_number = int(char)
+            else:
+                second_number = int(char)
+            
+    if first_number == -1 and second_number == -1:
+        first_number = 0
+        second_number = 0
+        print('no numbers found for ' + line)
+    
+    total += (first_number * 10) + second_number
+
+print(total)
