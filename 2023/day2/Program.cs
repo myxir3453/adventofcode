5eaefd2fa8e7f014da4685b0c7f3e363c12576ff
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-02 20:28:28 +0200

    day 2 setup

---
+++ 2023/day2/Program.cs
@@ -0,0 +1,11 @@
+﻿public record Game(int red, int green, int blue);
+
+public static class Program
+{
+    public static void Main()
+    {
+        Game game1 = new(2, 4, 1);
+
+        Console.WriteLine(game1);
+    }
+}
