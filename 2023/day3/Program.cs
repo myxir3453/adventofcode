1133c0735946aebc630daa31acf57ae7f32dd328
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-03 17:05:26 +0200

    day 3, part 1 using various data structures

---
+++ 2023/day3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode.Day3
+{
+
+    public class Program
+    {
+        private record Number(int Id, int Row, int Column, int Value);
+        private record Symbol(int Id, int Row, int Column, char Character);
+
+        public static void Main()
+        {
+            new Program().Run();
+        }
+
+        public void Run()
+        {
+            foreach (string line in File.ReadLines("testinput"))
+            {
+                MatchCollection numberMatches = Regex.Matches(line, @"[0-9]+");
+                foreach (Match match in numberMatches)
+                {
+                    
+                }
+            }
+        }
+    }
+}
