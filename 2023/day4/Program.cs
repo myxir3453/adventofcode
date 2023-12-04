b5f79886b4b6dc4e04d7df23df870d8a12431791
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-04 15:00:44 +0200

    refactor for day 4, part 2

--- 2023/day4/Program.cs
+++ 2023/day4/Program.cs
@@ -9,6 +9,8 @@ namespace AdventOfCode.Day4
 
     public class Program
     {
+        private record Card(int Number, int Matches);
+
         public static void Main()
         {
             new Program().Run();
@@ -16,41 +18,44 @@ namespace AdventOfCode.Day4
 
         private void Run()
         {
-            int totalWorth = 0;
-            //foreach (string line in File.ReadLines("testinput"))
-            foreach (string line in File.ReadLines("input"))
-            {
-                Match match = Regex.Match(line, @"Card ([0-9 ]+): ([0-9 ]+) \| ([0-9 ]+)");
-                int cardNumber = int.Parse(match.Groups[1].Value);
-                int[] winningNumbers = match.Groups[2].Value
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-                int[] numbersYouHave = match.Groups[3].Value
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(int.Parse)
-                    .ToArray();
-                int[] winningNumbersFreq = new int[100];
-                foreach (int winningNumber in winningNumbers)
-                {
-                    winningNumbersFreq[winningNumber]++;
-                }
-                int[] numbersYouHaveFreq = new int[100];
-                foreach (int numberYouHave in numbersYouHave)
-                {
-                    numbersYouHaveFreq[numberYouHave]++;
-                }
-                int matches = 0;
-                for (int i = 0;i < 100;i++)
-                {
-                    matches += (winningNumbersFreq[i] & numbersYouHaveFreq[i]) > 0 ? 1 : 0;
-                }
-                //Console.WriteLine($"card {cardNumber}: {matches} matches");
-                int worth = matches == 0 ? 0 : (1 << (matches - 1));
-                totalWorth += worth;
-            }
+            Card[] cards = File.ReadLines("input")
+                .Select(ProcessCard)
+                .ToArray();
 
+            // part 1
+            int totalWorth = cards.Sum(card => card.Matches == 0 ? 0 : (1 << (card.Matches - 1)));
             Console.WriteLine(totalWorth);
+        }
+
+        private Card ProcessCard(string line)
+        {
+            Match match = Regex.Match(line, @"Card ([0-9 ]+): ([0-9 ]+) \| ([0-9 ]+)");
+            int cardNumber = int.Parse(match.Groups[1].Value);
+            int[] winningNumbers = match.Groups[2].Value
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] numbersYouHave = match.Groups[3].Value
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] winningNumbersFreq = new int[100];
+            foreach (int winningNumber in winningNumbers)
+            {
+                winningNumbersFreq[winningNumber]++;
+            }
+            int[] numbersYouHaveFreq = new int[100];
+            foreach (int numberYouHave in numbersYouHave)
+            {
+                numbersYouHaveFreq[numberYouHave]++;
+            }
+            int matches = 0;
+            for (int i = 0;i < 100;i++)
+            {
+                matches += (winningNumbersFreq[i] & numbersYouHaveFreq[i]) > 0 ? 1 : 0;
+            }
+            //Console.WriteLine($"card {cardNumber}: {matches} matches");
+            return new Card(cardNumber, matches);
         }
     }
 }
