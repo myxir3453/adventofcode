05eb5cbbcb088387a7485b08183290502b195d39
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-12 12:08:46 +0200

    day 6, part 2

--- 2023/day6/day6.py
+++ 2023/day6/day6.py
@@ -1,21 +1,18 @@
 import re
 import math
+import functools
 
 lines = []
-with open('testinput', 'r') as fin:
+with open('input', 'r') as fin:
     lines = fin.readlines()
 
 race_times = [int(x) for x in re.findall("[0-9]+", lines[0])]
 race_distances = [int(x) for x in re.findall("[0-9]+", lines[1])]
 
-nr_races = len(race_times)
+bigrace_time = int(functools.reduce(lambda x, y : x + y, re.findall("[0-9]+", lines[0])))
+bigrace_distance = int(functools.reduce(lambda x, y : x + y, re.findall("[0-9]+", lines[1])))
 
-part1 = 1
-
-for i in range(nr_races):
-    rt = race_times[i]
-    rd = race_distances[i]
-
+def find_nr_race_solutions(rt, rd):
     x1 = (-rt + math.sqrt(rt ** 2 - 4 * rd)) / (-2)
     x2 = (-rt - math.sqrt(rt ** 2 - 4 * rd)) / (-2)
 
@@ -24,6 +21,15 @@ for i in range(nr_races):
     
     nr_solutions = last_good_value - first_good_value + 1
 
-    part1 *= nr_solutions
+    return nr_solutions
 
+nr_races = len(race_times)
+
+part1 = functools.reduce(
+    lambda x, y : x * y,
+    [find_nr_race_solutions(race_times[i], race_distances[i])
+        for i in range(nr_races)])
 print(part1)
+
+part2 = find_nr_race_solutions(bigrace_time, bigrace_distance)
+print(part2)
