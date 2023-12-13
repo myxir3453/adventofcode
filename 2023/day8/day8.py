348fd950e23ce3429470d99cbc521a978a6b8abb
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-13 16:48:39 +0200

    day 8, part 2

--- 2023/day8/day8.py
+++ 2023/day8/day8.py
@@ -1,5 +1,16 @@
 import re
-#import queue
+from functools import reduce
+
+def gcd(a, b):
+    while b:      
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+def lcmm(args):
+    return reduce(lcm, args)
 
 with open('input', 'r') as fin:
     lines = [line.rstrip() for line in fin.readlines()]
@@ -8,34 +19,46 @@ ions = [ion for ion in lines[0]]
 node_indexes = {}
 
 nodes = []
+starting_nodes = []
+ending_nodes = []
 for i in range(2, len(lines)):
-    match = re.search(r'([A-Z]+) = \(([A-Z]+), ([A-Z]+)\)', lines[i])
+    # print(lines[i])
+    match = re.search(r'([A-Z0-9]+) = \(([A-Z0-9]+), ([A-Z0-9]+)\)', lines[i])
     src = match.group(1)
     left = match.group(2)
     right = match.group(3)
     nodes.append((src, left, right))
     node_indexes[src] = i - 2
+    if src[-1] == 'A':
+        starting_nodes.append(i - 2)
+    if src[-1] == 'Z':
+        ending_nodes.append(i - 2)
 nodes = [(node[0], node_indexes[node[1]], node_indexes[node[2]]) for node in nodes]
-#print(nodes)
+# print(nodes)
 
-nsteps = 0
-cnode = nodes[node_indexes['AAA']]
-cion = 0
-while cnode[0] != 'ZZZ':
-    # print('current node: ' + cnode[0])
-    # print('next instruction: ' + ions[cion])
+nstepsm = []
+for starting_node in starting_nodes:
+    nsteps = 0
+    cnode = nodes[starting_node]
+    # print('starting node: ' + cnode[0])
+    cion = 0
+    while cnode[0][-1] != 'Z':
+        # print('current nodes: ' + str(cnodes))
+        # print('next instruction: ' + ions[cion])
 
-    if ions[cion] == 'R':
-        cnode = nodes[cnode[2]]
-    else:
-        cnode = nodes[cnode[1]]
+        if ions[cion] == 'R':
+            cnode = nodes[cnode[2]]
+        else:
+            cnode = nodes[cnode[1]]
 
-    if cion < len(ions) - 1:
-        cion += 1
-    else:
-        cion = 0
+        nsteps += 1
 
-    nsteps += 1
-# print('current node: ' + cnode[0])
+        if cion < len(ions) - 1:
+            cion += 1
+        else:
+            cion = 0
+    # print('current nodes: ' + str(cnodes))
 
-print('steps: ' + str(nsteps))
+    nstepsm.append(nsteps)
+
+print(lcmm(nstepsm))
