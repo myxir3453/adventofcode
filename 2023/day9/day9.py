1e52e0e7a92131142b637224b03a13597e6c53b5
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-13 17:59:58 +0200

    day 9, part 2

--- 2023/day9/day9.py
+++ 2023/day9/day9.py
@@ -1,22 +1,23 @@
 with open('input', 'r') as fin:
     lines = fin.readlines()
 
-p = 0
+sum = 0
 
 for line in lines:
     h = [int(x) for x in line.rstrip().split()]
-    step = 0
+
+    stack = []
     while any([x != 0 for x in h]):
-        # print(h)
-
-        p += h[-1]
-
-        for i in range(len(h) - 1, 0, -1):
-            h[i] -= h[i-1]
+        stack.append(h[0])
+        for i in range(0, len(h) - 1):
+            h[i] = h[i + 1] - h[i]
         
-        h.pop(0)
+        h.pop(-1)
 
-        step += 1
-    # print(h)
+    p = stack.pop(-1)
+    while len(stack) > 0:
+        p = stack.pop(-1) - p
+    
+    sum += p
 
-print(p)
+print(sum)
