7434e7669ecde8d4616bf07d9d2372bb63895b32
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-15 19:54:05 +0200

    day 12, part 2 done

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -1,57 +1,50 @@
 import re
 
+cache = {}
+
+def count_arrangements(record1, record2):
+    # print((record1, record2))
+    if record2 == ():
+        return 1 if '#' not in record1 else 0
+    
+    res = 0
+    
+    # find first sliding window from record1 
+    # that matches record2[0]; 
+    # stop where rest of record1 would match with rest of record2
+    swlen = record2[0]
+    record2_remainder_length = sum(record2[1:]) + (len(record2) - 1)
+    swend = len(record1) - swlen - record2_remainder_length
+
+    for swstart in range(0, swend):
+        window = record1[swstart : swstart + swlen]
+        #print(((window, record1[swstart + swlen + 1:]), (record2[0], record2[1:])))
+        if record1[swstart + swlen] == '#':
+            continue
+        if '#' in record1[:swstart]:
+            break
+        if '.' in window:
+            continue
+        nextargs = (record1[swstart + swlen + 1:], record2[1:])
+        if nextargs not in cache:
+            cache[nextargs] = count_arrangements(*nextargs)
+        res += cache[nextargs]
+    
+    return res
+
 with open('input', 'r') as fin:
     lines = [line.rstrip() for line in fin.readlines()]
 
-totalarr = 0
+total_arrangements = 0
 
-for lineidx in range(len(lines)):
-    print('line ' + str(lineidx + 1) + ' / ' + str(len(lines)))
-    line = lines[lineidx]
-    record1 = line.split(' ')[0]
-    record2 = [int(x) for x in line.split(' ')[1].split(',')]
+for line in lines:
+    record1, record2 = line.split()
+    record2 = tuple(map(int, record2.split(',')))
+    record2 = record2 * 5
+    record1 = '?'.join([record1] * 5)
+    record1 += '.'
+    cache = {}
+    total_arrangements += count_arrangements(record1, record2)
 
-    nr_screws = len(record1)
-
-    record1 = record1.replace('.', '0')
-    record1 = record1.replace('#', '1')
-    # print(record1)
-
-    masks_for_unknowns = []
-    nr_unknowns = 0
-    for un in re.finditer(r'(\?)', record1):
-        mask = 2 ** (nr_screws - un.start(0) - 1)
-        masks_for_unknowns.append(2 ** (nr_screws - un.start(0) - 1))
-        nr_unknowns += 1
-    masks_for_unknowns.reverse()
-    # for mask in masks_for_unknowns:
-    #     print(bin(mask)[2:].rjust(nr_screws, '0'))
-
-    record1_base = int(record1.replace('?', '0'), base=2)
-    # print(bin(record1)[2:].rjust(nr_screws, '0'))
-
-    narr = 0
-
-    # print()
-    for k in range(2**nr_unknowns):
-        #print(bin(k)[2:].rjust(nr_unknowns, '0'))
-
-        record1 = record1_base
-        for bit_index in range(nr_unknowns):
-            #bit = (k & (1 << (nr_unknowns - bit_index - 1))) >> (nr_unknowns - bit_index - 1)
-            bit = (k & (1 << bit_index)) >> bit_index
-            #print(bit)
-            if bit == 1:
-                record1 |= masks_for_unknowns[bit_index]
-        
-        record1_binary = bin(record1)[2:].rjust(nr_screws, '0')
-        
-        occs = [len(occ) for occ in re.findall(r'1+', record1_binary)]
-
-        if occs == record2:
-            narr += 1
-
-    #print(str(narr) + ' arrangements')
-    totalarr += narr
-
-print(totalarr)
+print(len(cache))
+print(total_arrangements)
