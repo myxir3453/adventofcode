4b0c261e580ba7185e7873afcf69be6b943d5a76
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-23 15:08:31 +0200

    day 16, part 1 - recursive solution, works for testinput but exceeds recursion limit for input

---
+++ 2023/day16/day16.py
@@ -0,0 +1,68 @@
+with open('input', 'r') as fin:
+    grid = [list(line.rstrip()) for line in fin.readlines()]
+nrows = len(grid)
+ncols = len(grid[0])
+visited = set()
+
+def go_right(pos):
+    visit((pos[0], pos[1] + 1), (0, 1))
+def go_left(pos):
+    visit((pos[0], pos[1] - 1), (0, -1))
+def go_up(pos):
+    visit((pos[0] - 1, pos[1]), (-1, 0))
+def go_down(pos):
+    visit((pos[0] + 1, pos[1]), (1, 0))
+def go_on(pos, dir):
+    visit((pos[0] + dir[0], pos[1] + dir[1]), dir)
+
+def visit(pos, dir):
+    if pos[0] not in range(nrows) or \
+       pos[1] not in range(ncols):
+        return
+    encounter = grid[pos[0]][pos[1]]
+    if (pos, dir) in visited:
+        return
+    visited.add((pos, dir))
+    # print((pos[0] + 1, pos[1] + 1), dir)
+    if encounter == '.':
+        go_on(pos, dir)
+    if encounter == '|':
+        if dir == (0, 1) or dir == (0, -1):
+            go_up(pos)
+            go_down(pos)
+        else:
+            go_on(pos, dir)
+    if encounter == '-':
+        if dir == (1, 0) or dir == (-1, 0):
+            go_right(pos)
+            go_left(pos)
+        else:
+            go_on(pos, dir)
+    if encounter == '/':
+        if dir == (0, 1):
+            go_up(pos)
+        if dir == (0, -1):
+            go_down(pos)
+        if dir == (1, 0):
+            go_left(pos)
+        if dir == (-1, 0):
+            go_right(pos)
+    if encounter == '\\':
+        if dir == (0, 1):
+            go_down(pos)
+        if dir == (0, -1):
+            go_up(pos)
+        if dir == (1, 0):
+            go_right(pos)
+        if dir == (-1, 0):
+            go_left(pos)
+
+visit((0, 0), (0, 1))
+
+print(len(set(pos for pos, dir in visited)))
+
+# grid2 = [['.' for col in range(ncols)] for row in range(nrows)]
+# for pos, dir in visited:
+#     print(pos)
+#     grid2[pos[0]][pos[1]] = '#'
+# [print(''.join(line)) for line in grid2]
