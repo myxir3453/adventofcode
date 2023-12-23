ece44d46f6ae55b4a00407d14e67e96e7c484750
Attila Geiszt <attila.geiszt@gmail.com>
2023-12-23 15:32:41 +0200

    day 16, part 2 done

--- 2023/day16/day16.py
+++ 2023/day16/day16.py
@@ -2,26 +2,25 @@ with open('input', 'r') as fin:
     grid = [list(line.rstrip()) for line in fin.readlines()]
 nrows = len(grid)
 ncols = len(grid[0])
-visited = set()
-stack = []
 
-def go_right(pos):
+def go_right(pos, stack):
     #visit((pos[0], pos[1] + 1), (0, 1))
     stack.append(((pos[0], pos[1] + 1), (0, 1)))
-def go_left(pos):
+def go_left(pos, stack):
     #visit((pos[0], pos[1] - 1), (0, -1))
     stack.append(((pos[0], pos[1] - 1), (0, -1)))
-def go_up(pos):
+def go_up(pos, stack):
     #visit((pos[0] - 1, pos[1]), (-1, 0))
     stack.append(((pos[0] - 1, pos[1]), (-1, 0)))
-def go_down(pos):
+def go_down(pos, stack):
     #visit((pos[0] + 1, pos[1]), (1, 0))
     stack.append(((pos[0] + 1, pos[1]), (1, 0)))
-def go_on(pos, dir):
+def go_on(pos, dir, stack):
     #visit((pos[0] + dir[0], pos[1] + dir[1]), dir)
     stack.append(((pos[0] + dir[0], pos[1] + dir[1]), dir))
-
 def solve(startpos, startdir):
+    visited = set()
+    stack = []
     stack.append((startpos, startdir))
     while len(stack) > 0:
         pos, dir = stack.pop()
@@ -34,41 +33,49 @@ def solve(startpos, startdir):
         visited.add((pos, dir))
         # print((pos[0] + 1, pos[1] + 1), dir)
         if encounter == '.':
-            go_on(pos, dir)
+            go_on(pos, dir, stack)
         if encounter == '|':
             if dir == (0, 1) or dir == (0, -1):
-                go_up(pos)
-                go_down(pos)
+                go_up(pos, stack)
+                go_down(pos, stack)
             else:
-                go_on(pos, dir)
+                go_on(pos, dir, stack)
         if encounter == '-':
             if dir == (1, 0) or dir == (-1, 0):
-                go_right(pos)
-                go_left(pos)
+                go_right(pos, stack)
+                go_left(pos, stack)
             else:
-                go_on(pos, dir)
+                go_on(pos, dir, stack)
         if encounter == '/':
             if dir == (0, 1):
-                go_up(pos)
+                go_up(pos, stack)
             if dir == (0, -1):
-                go_down(pos)
+                go_down(pos, stack)
             if dir == (1, 0):
-                go_left(pos)
+                go_left(pos, stack)
             if dir == (-1, 0):
-                go_right(pos)
+                go_right(pos, stack)
         if encounter == '\\':
             if dir == (0, 1):
-                go_down(pos)
+                go_down(pos, stack)
             if dir == (0, -1):
-                go_up(pos)
+                go_up(pos, stack)
             if dir == (1, 0):
-                go_right(pos)
+                go_right(pos, stack)
             if dir == (-1, 0):
-                go_left(pos)
+                go_left(pos, stack)
+    return len(set(pos for pos, dir in visited))
 
-solve((0, 0), (0, 1))
+starts = []
+for col in range(ncols):
+    starts.append(((0, col), (1, 0)))
+    starts.append(((nrows - 1, col), (-1, 0)))
+for row in range(nrows):
+    starts.append(((row, 0), (0, 1)))
+    starts.append(((row, ncols-1), (0, -1)))
 
-print(len(set(pos for pos, dir in visited)))
+maxenergy = max([solve(spos, sdir) for spos, sdir in starts])
+print(maxenergy)
 
 # grid2 = [['.' for col in range(ncols)] for row in range(nrows)]
 # for pos, dir in visited:
